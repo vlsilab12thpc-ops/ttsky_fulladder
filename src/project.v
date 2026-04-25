23d356b60739ee82427bc3cff69927e7ceb40e59
vlsilab12thpc-ops <vlsilab12thpc@gmail.com>
2026-04-25 11:24:19 +0530

    Consolidate output pin assignment for clarity

--- src/project.v
+++ src/project.v
@@ -29,8 +29,7 @@ module tt_um_example (
      
 
   // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out [7:2] = 0;
-    assign ui_in [7:3] = 0;    // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out [7:2] = 0;    // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out = 0;
     assign uio_oe  = 0;
 
